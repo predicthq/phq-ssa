feae4478def41bf74223aa53bc89223c5d9f8554
XipingFu <57967798+XipingFu@users.noreply.github.com>
2020-01-09 09:25:06 +1300

    Remove redundant variables and unnecessary computations

--- phq/ssa.py
+++ phq/ssa.py
@@ -3,7 +3,6 @@ import logging
 
 import numpy as np
 import pandas as pd
-from pandas import DataFrame as df
 from scipy import linalg
 
 log = logging.getLogger(__name__)
@@ -17,23 +16,15 @@ class SSA:
         self._timeseries_data = pd.DataFrame(time_series)
 
     @staticmethod
-    def _dot(x, y):
-        """
-        Alternative formulation of dot product to allow missing values in arrays/matrices
-        """
-        pass
-
-    @staticmethod
     def get_contributions(singular_values):
         """
-        Calculate the relative contribution of each of the singular values
+        Calculate the relative contribution of each of the positive singular values
         :param singular_values: numpy array
         :return: Dataframe with positive Contribution values
         """
         lambdas = np.power(singular_values, 2)
-        contributions_df = df((lambdas / lambdas.sum()).round(4), columns=['Contribution'])
-        # Only returns positive contribution values
-        return contributions_df[contributions_df.Contribution > 0]
+        contributions = lambdas / lambdas.sum()                
+        return contributions[contributions> 0]
 
     @staticmethod
     def diagonal_averaging(trajectory_matrix):
@@ -42,29 +33,26 @@ class SSA:
         :param trajectory_matrix: numpy 2D array
         :return: Pandas DataFrame object containing the reconstructed series
         """
-        matrix = np.matrix(trajectory_matrix)
-        row_count, col_count = matrix.shape
+        row_count, col_count = trajectory_matrix.shape
 
         if row_count > col_count:
             # Transpose the matrix
-            matrix = np.matrix.T
+            trajectory_matrix = trajectory_matrix.T
             row_count, col_count = col_count, row_count
 
         ret = []
         # Diagonal Averaging
         for k in range(1 - col_count, row_count):
             mask = np.eye(col_count, k=k, dtype='bool')[::-1][:row_count, :]
-            ma = np.ma.masked_array(matrix.A, mask=(1 - mask))
+            ma = np.ma.masked_array(trajectory_matrix.A, mask=(1 - mask))
             ret += [ma.sum() / mask.sum()]
 
-        return df(ret, columns=['Reconstruction'])
+        return pd.DataFrame(ret, columns=['Reconstruction'])
 
-    def embed(self, embedding_dimension=None, suspected_frequency=None):
+    def embed(self, embedding_dimension=None):
         """
         Embed the time series with embedding_dimension window size.
-        Optional: suspected_frequency changes embedding_dimension such that it is divisible by suspected frequency'''
         :param embedding_dimension:
-        :param suspected_frequency:
         :return:
         """
         timeseries_data_row_count = self._timeseries_data.shape[0]
@@ -72,32 +60,17 @@ class SSA:
         if not embedding_dimension:
             embedding_dimension = timeseries_data_row_count // 2
 
-        if suspected_frequency:
-            embedding_dimension = (embedding_dimension // suspected_frequency) * suspected_frequency
-
         log.debug('Embedding dimension: %(dimension)s', {'dimension': embedding_dimension})
 
         k = timeseries_data_row_count - embedding_dimension + 1
         x = np.matrix(linalg.hankel(self._timeseries_data, np.zeros(embedding_dimension))).T[:, :k]
-        x_df = df(x)
-        log.debug('Trajectory dimensions: %(dimension)s', {'dimension': x_df.shape})
+        return x
 
-        x_complete = x_df.dropna(axis=1).values
-        log.debug('Complete dimensions: %(dimension)s', {'dimension': x_complete.shape})
-
-        x_missing = x_df.drop(x_complete.columns, axis=1)
-        log.debug('Missing dimensions: %(dimension)s', {'dimension': x_missing.shape})
-
-        return {
-            'x': x_df,
-            'x_complete': x_complete,
-            'x_missing': x_missing
-        }
-
-    def decompose(self, x):
+    def reconstruction(self, x, contributionprop):
         """
-        Perform the Singular Value Decomposition and identify the rank of the embedding subspace
-        Characteristic of projection: the proportion of variance captured in the subspace
+        Reconstruction based on the trajectory matrix/matrices and contributionprop 
+        :param x: trajectory_matrix
+        :param contributionprop: the percentage of energy for reconstruction
         """
         u, s, v = linalg.svd(x * x.T)
         u = np.matrix(u)
@@ -105,26 +78,23 @@ class SSA:
         v = np.matrix(v)
         d = np.linalg.matrix_rank(x)
         log.debug('Dimension of projection space: %(dimension)d', {'dimension': d})
-
-        vs, xs, ys, zs = {}, {}, {}, {}
-        for i in range(d):
-            zs[i] = s[i] * v[:, i]
-            vs[i] = x.T * (u[:, i] / s[i])
-            ys[i] = s[i] * u[:, i]
-            xs[i] = ys[i] * np.matrix(vs[i]).T
-
-        contributions = self.get_contributions(s)
-        r = len(contributions[contributions > 0])
+        
+        ssa_s_contributions = self.get_contributions(s)
+        sum_ssa = []
+        for i in range(ssa_s_contributions.shape[0]):
+            a =sum(ssa_s_contributions[0:i])
+            sum_ssa.append(a) 
+        t1 = [i - contributionprop for i in sum_ssa]
+        t2 = np.abs(t1)
+        nsig = np.argmin(t2)
+        xs = np.zeros(x.shape)
+        for i in range(nsig):
+            vi = x.T * (u[:, i] / s[i])
+            yi = s[i] * u[:, i]
+            xs = xs + yi * np.matrix(vi).T
+        r = len(ssa_s_contributions)
         log.debug('Rank of trajectory: %(rank)d', {'rank': r})
 
         characteristic = round((s[:r] ** 2).sum() / (s ** 2).sum(), 4)
         log.debug('Characteristic of projection: %(characteristics)f', {'characteristics': characteristic})
-
-        return vs, xs, ys, zs
-
-    def reconstruction(self, *trajectory_matrices):
-        """
-        Reconstruction of the trajectory matrix/matrices
-        :param trajectory_matrices: (List of) Numpy 2D array
-        """
-        return self.diagonal_averaging(reduce(np.add, trajectory_matrices))
+        return self.diagonal_averaging(xs), nsig
